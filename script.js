8ee6bdd4f9259974b06fd822dc2236462ad21767
Edis Hulpea <edis.hulpea@gmail.com>
2021-02-16 03:59:03 +0200

    Experimented with jquerry by fetching json

--- script.js
+++ script.js
@@ -2,25 +2,20 @@
 const staticUrl =
   "https://raw.githubusercontent.com/Edis-over/first-responsive/main/images/images.json";
 $.getJSON(staticUrl, function (data) {
-  class PopulateImages {
-    constructor() {
-      this.bottomFlex = document.querySelector(".bottom-flex");
-    }
-    printImages() {
-      data.bottomFlexImages.forEach((element) => {
-        const createDiv = document.createElement("div");
-        createDiv.classList.add(
-          "col-lg-3",
-          "col-md-4",
-          "col-sm-6",
-          "col-xs-12"
-        );
-        const createA = document.createElement("a");
-        createDiv.appendChild(createA);
-      });
-    }
-  }
-  const addImages = new PopulateImages();
+  this.bottomFlex = document.querySelector(".bottom-flex");
+  $.each(data, function () {
+    // const bottomFlex = document.querySelector(".bottom-flex");
+    const createDiv = document.createElement("div");
+    createDiv.classList.add("col-lg-3", "col-md-4", "col-sm-6", "col-xs-12");
+    const createA = document.createElement("a");
+    const createImg = document.createElement("img");
+    createImg.src = data.bottomFlex;
+    let appendImgA = createA.appendChild(createImg);
+    let appendDivImgA = createDiv.appendChild(createA);
+    console.log(appendDivImgA);
+    this.bottomFlex.appendChild(createDiv);
+    console.log(data);
+  });
 });
 
 // //burger menu open/close
