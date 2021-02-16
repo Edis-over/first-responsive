c812c71a0cfcf91a16f5a8fe48685181a7f5bf93
Edis Hulpea <edis.hulpea@gmail.com>
2021-02-16 02:49:31 +0200

    small changes made to the js and the json

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Get images for bot flex
+const staticUrl =
+  "https://raw.githubusercontent.com/Edis-over/first-responsive/main/images/images.json";
+$.getJSON(staticUrl, function (data) {
+  class PopulateImages {
+    constructor() {
+      this.bottomFlex = document.querySelector(".bottom-flex");
+    }
+    printImages() {
+      data.bottomFlexImages.forEach((element) => {
+        const createDiv = document.createElement("div");
+        createDiv.classList.add(
+          "col-lg-3",
+          "col-md-4",
+          "col-sm-6",
+          "col-xs-12"
+        );
+        const createA = document.createElement("a");
+        createDiv.appendChild(createA);
+      });
+    }
+  }
+  const addImages = new PopulateImages();
+});
+
 // //burger menu open/close
 
 class Burger {
