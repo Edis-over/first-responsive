92d4d0dc34a810e34baf7a18998d165c555dd5a3
Edis Hulpea <edis.hulpea@gmail.com>
2021-02-16 19:28:22 +0200

    fixed image loading from json

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-// Get images for bot flex
-const staticUrl =
-  "https://raw.githubusercontent.com/Edis-over/first-responsive/main/images/images.json";
-$.getJSON(staticUrl, function (data) {
-  this.bottomFlex = document.querySelector(".bottom-flex");
-  $.each(data, function () {
-    // const bottomFlex = document.querySelector(".bottom-flex");
-    const createDiv = document.createElement("div");
-    createDiv.classList.add("col-lg-3", "col-md-4", "col-sm-6", "col-xs-12");
-    const createA = document.createElement("a");
-    const createImg = document.createElement("img");
-    createImg.src = data.bottomFlex;
-    let appendImgA = createA.appendChild(createImg);
-    let appendDivImgA = createDiv.appendChild(createA);
-    console.log(appendDivImgA);
-    this.bottomFlex.appendChild(createDiv);
-    console.log(data);
+// GET images for bot flex
+fetch(
+  "https://raw.githubusercontent.com/Edis-over/first-responsive/main/images/images.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const arraySelect = data.bottomFlexImages;
+    let allImages = "";
+    for (i = 0; i < arraySelect.length; i++) {
+      let itemsToBeAdded = `<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <a href="#"> <img src="${arraySelect[i].url}" alt="${arraySelect[i].alt}"></img></a>
+    </div>`;
+      allImages += itemsToBeAdded;
+    }
+    document.querySelector(".bottom-flex").innerHTML = allImages;
   });
-});
+
+// Too much code does same shit
+// const bottomFlex = document.querySelector(".bottom-flex");
+//       const createDiv = document.createElement("div");
+//       createDiv.classList.add("col-lg-3", "col-md-4", "col-sm-6", "col-xs-12");
+//       const createA = document.createElement("a");
+//       const createImg = document.createElement("img");
+//       createImg.src = data.bottomFlexImages[i].url;
+//       let appendImgA = createA.appendChild(createImg);
+//       let appendDivImgA = createDiv.appendChild(createA);
+//       bottomFlex.appendChild(createDiv);
 
 // //burger menu open/close
 
